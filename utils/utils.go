2b51acbfc05fd6b482c7ec44a18f2a1457b8187f
Mitchell Grenier <mitchell.grenier@bitmex.com>
2019-10-03 10:10:04 -0700

    Add JSON Printing by default
    This is just a quick hack to make printing nice. It needs enums and better handling of escaping strings containing " but it's a start and makes the output actually bearable to look at. It also makes it somewhat presentable.

---
+++ utils/utils.go
@@ -0,0 +1,25 @@
+// Holds utility functions like print formatting or conversion functions
+package utils
+
+import (
+	"fmt"
+	"encoding/json"
+)
+
+func prettyPrintQueryResultsJSON(results []map[string]string) {
+	formatted, err := json.MarshalIndent(results, "", "    ")
+	if err != nil {
+		fmt.Printf("Could not format query results.\n");
+		return
+	}
+	fmt.Printf("%s\n", formatted)
+}
+
+func PrettyPrintQueryResults(results []map[string]string, format int) {
+	switch(format) {
+		case 0:
+			prettyPrintQueryResultsJSON(results)
+		default:
+	}
+}
+
