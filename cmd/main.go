52906fe5cce1f8e5d410c1f9dd60feb07bcdee80
Andrew Guthrie <andrew-guthrie@live.com>
2019-10-03 23:13:27 -0700

    Implementation of persistent command history with home folder file

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path"
 	"strings"
 
 	"github.com/AbGuthrie/goquery/commands"
@@ -9,6 +14,82 @@ import (
 
 	prompt "github.com/c-bata/go-prompt"
 )
+
+var historyPath string
+
+func init() {
+	// Populate historyPath and create history file if needed
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(fmt.Printf("Failed to fetch user info for home directory: %s", err))
+	}
+	goQueryPath := path.Join(usr.HomeDir, ".goquery")
+	historyPath = path.Join(goQueryPath, "history")
+
+	// Create directory and file home if it doesn't exist yet
+	if _, err := os.Stat(historyPath); os.IsNotExist(err) {
+		os.MkdirAll(goQueryPath, os.ModePerm)
+		emptyHistory, err := os.Create(historyPath)
+		emptyHistory.Close()
+		if err != nil {
+			log.Fatal(fmt.Printf("Failed to create history file: %s", err))
+		}
+	}
+}
+
+func main() {
+	history, err := loadHistoryFile()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p := prompt.New(
+		executor,
+		completer,
+		prompt.OptionPrefix("goquery> "),
+		prompt.OptionLivePrefix(refreshLivePrefix),
+		prompt.OptionTitle("goquery"),
+		prompt.OptionHistory(history),
+	)
+	p.Run()
+}
+
+// Begin history file functions
+// ----------------------------
+
+func loadHistoryFile() ([]string, error) {
+	historyBytes, err := ioutil.ReadFile(historyPath)
+	if err != nil {
+		return []string{}, err
+	}
+	lines := strings.Split(string(historyBytes), "\n")
+	return lines, nil
+}
+
+func updateHistoryFile(line string) error {
+	// If history file is empty, don't prepend \n to entry
+	newline := "\n"
+	historyBytes, err := ioutil.ReadFile(historyPath)
+	if err != nil {
+		return err
+	}
+	if string(historyBytes) == "" {
+		newline = ""
+	}
+	// Write line entry to history file
+	historyFile, err := os.OpenFile(historyPath, os.O_APPEND|os.O_WRONLY, 0644)
+	defer historyFile.Close()
+	if err != nil {
+		return err
+	}
+	if _, err := historyFile.Write([]byte(newline + line)); err != nil {
+		return err
+	}
+	return nil
+}
+
+// begin go-prompt integration functions
+// ------------------------------------
 
 func refreshLivePrefix() (string, bool) {
 	// Prototype for showing current connected host state in
@@ -31,6 +112,9 @@ func executor(input string) {
 	} else {
 		fmt.Printf("No such command: %s\n", args[0])
 	}
+	if err := updateHistoryFile(input); err != nil {
+		fmt.Printf("%s\n", err)
+	}
 }
 
 func completer(in prompt.Document) []prompt.Suggest {
@@ -39,15 +123,4 @@ func completer(in prompt.Document) []prompt.Suggest {
 		return []prompt.Suggest{}
 	}
 	return prompt.FilterHasPrefix(commands.SuggestionsMap, w, true)
-}
-
-func main() {
-	p := prompt.New(
-		executor,
-		completer,
-		prompt.OptionPrefix("goquery> "),
-		prompt.OptionLivePrefix(refreshLivePrefix),
-		prompt.OptionTitle("goquery"),
-	)
-	p.Run()
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ require (
 	github.com/c-bata/go-prompt v0.2.3
 	github.com/mattn/go-runewidth v0.0.4 // indirect
 	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
+	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
+	golang.org/x/net v0.0.0-20191003171128-d98b1b443823 // indirect
 )
 
 go 1.13
