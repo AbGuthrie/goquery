6b4f6ae577761d6b0743e1302b5db65e063f10d7
Mitchell Grenier <mitchell@confurious.io>
2019-10-07 01:01:29 -0700

    Add a host history command

---
+++ commands/history.go
@@ -0,0 +1,29 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/AbGuthrie/goquery/hosts"
+)
+
+
+func history(cmdline string) error {
+	args := strings.Split(cmdline, " ") // Separate command and arguments
+	if len(args) > 1 {
+		return fmt.Errorf("History takes no parameters")
+	}
+
+	host, err := hosts.GetCurrentHost()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Query Name : Query\n")
+	for _, query := range host.QueryHistory {
+		fmt.Printf("%s: %s\n", query.Name, query.SQL)
+	}
+
+	return nil
+}
