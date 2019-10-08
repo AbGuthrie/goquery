de61fff5a2ea863f0f1b51d59b4a2de63a5fa48d
Mitchell Grenier <mitchell.grenier@bitmex.com>
2019-10-07 17:16:10 -0700

    Implementing SSO Support and Infra

--- go.mod
+++ go.mod
@@ -1,9 +1,15 @@
 module github.com/AbGuthrie/goquery
 
 require (
+	github.com/beevik/etree v1.1.0 // indirect
 	github.com/c-bata/go-prompt v0.2.3
+	github.com/crewjam/saml v0.0.0-20190521120225-344d075952c9 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jonboulle/clockwork v0.1.0 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
 	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
+	github.com/russellhaering/goxmldsig v0.0.0-20180430223755-7acd5e4a6ef7 // indirect
+	github.com/zenazn/goji v0.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20191002192127-34f69633bfdc // indirect
 	golang.org/x/net v0.0.0-20191003171128-d98b1b443823 // indirect
 )
--- goserver/mock_osquery_server.go
+++ goserver/mock_osquery_server.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"bytes"
+    "crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/xml"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
+
+	"github.com/crewjam/saml/samlsp"
 )
 
 type Query struct {
@@ -311,14 +319,45 @@ func fetchResults(w http.ResponseWriter, r *http.Request) {
 
 // End goquery APIs
 
+
+func doPut(url string, metadata string) error {
+	client := &http.Client{}
+	request, err := http.NewRequest("PUT", url, strings.NewReader(metadata))
+	request.ContentLength = int64(len(metadata))
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return err
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return err
+		}
+		fmt.Println("The calculated length is:", len(string(contents)), "for the url:", url)
+		fmt.Println("   ", response.StatusCode)
+		hdr := response.Header
+		for key, value := range hdr {
+			fmt.Println("   ", key, ":", value)
+		}
+		fmt.Println(contents)
+	}
+	return nil
+}
+
 func main() {
 	ENROLL_SECRET = "somepresharedsecret"
+	enableSSO := true
 	enrolledHosts = make(map[string]Host)
 	queryMap = make(map[string]map[string]Query)
 
 	// Set up flags for certs
 	serverCrt := flag.String("server_cert", "certs/example_server.crt", "Location of a certificate to use")
 	serverKey := flag.String("server_key", "certs/example_server.key", "Location of key for certificate")
+
+	ssoCrt := flag.String("sso_cert", "certs/example_goserver_sso.crt", "Location of a certificate to use for sso")
+	ssoKey := flag.String("sso_key", "certs/example_goserver_sso.key", "Location of key for certificate for sso")
 
 	flag.Parse()
 
@@ -330,10 +369,62 @@ func main() {
 	http.HandleFunc("/distributedWrite", distributedWrite)
 
 	// goquery Endpoints
-	http.HandleFunc("/checkHost", checkHost)
-	http.HandleFunc("/scheduleQuery", scheduleQuery)
-	http.HandleFunc("/fetchResults", fetchResults)
+	if enableSSO {
+		keyPair, err := tls.LoadX509KeyPair(*ssoCrt, *ssoKey)
+		if err != nil {
+			fmt.Printf("Could not load certificates for SSO\n")
+			panic(err)
+		}
 
+		keyPair.Leaf, err = x509.ParseCertificate(keyPair.Certificate[0])
+		if err != nil {
+			panic(err)
+		}
+
+		idpMetadataURL, err := url.Parse("http://goserversaml:8002/metadata")
+		if err != nil {
+			panic(err)
+		}
+
+		rootURL, err := url.Parse("https://localhost:8001")
+		if err != nil {
+			panic(err)
+		}
+
+		samlSP, _ := samlsp.New(samlsp.Options{
+			URL:            *rootURL,
+			Key:            keyPair.PrivateKey.(*rsa.PrivateKey),
+			Certificate:    keyPair.Leaf,
+			IDPMetadataURL: idpMetadataURL,
+			AllowIDPInitiated: true,
+		})
+
+		// Add to service to IDP for some reason happens via client call
+		fmt.Printf("Registering ourselves with the IDP Service\n")
+		var b bytes.Buffer
+		enc := xml.NewEncoder(&b)
+		samlSP.ServiceProvider.Metadata().MarshalXML(enc, xml.StartElement{});
+		for {
+			err = doPut("http://goserversaml:8002/services/:goserver", b.String())
+			if err == nil {
+				break
+			}
+		}
+		fmt.Printf("Registered ourselves with the IDP Service\n")
+
+		ch := http.HandlerFunc(checkHost)
+		sq := http.HandlerFunc(scheduleQuery)
+		fr := http.HandlerFunc(fetchResults)
+
+		http.Handle("/checkHost", samlSP.RequireAccount(ch))
+		http.Handle("/scheduleQuery", samlSP.RequireAccount(sq))
+		http.Handle("/fetchResults", samlSP.RequireAccount(fr))
+		http.Handle("/saml/", samlSP)
+	} else {
+		http.HandleFunc("/checkHost", checkHost)
+		http.HandleFunc("/scheduleQuery", scheduleQuery)
+		http.HandleFunc("/fetchResults", fetchResults)
+	}
 	fmt.Printf("Starting test goquery/osquery backend...\n")
 	fmt.Printf("Server Cert Path: %s\n", *serverCrt)
 	fmt.Printf("Server Key Path:  %s\n", *serverKey)
