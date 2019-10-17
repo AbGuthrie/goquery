8f7da537954fe7c76d1d96442c8b7bcd888fa40e
Andrew Guthrie <andrew-guthrie@live.com>
2019-10-16 23:28:42 -0700

    Implement .clear command

---
+++ commands/clear.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func clear(cmdline string) error {
+	args := strings.Split(cmdline, " ") // Separate command and arguments
+	if len(args) > 1 {
+		return fmt.Errorf("Clear takes no parameters")
+	}
+
+	currentOS := runtime.GOOS
+
+	if currentOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		print("\033[H\033[2J")
+	}
+	return nil
+}
